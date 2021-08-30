5599f118eef51a088f19f6f1c62fcd006ebf9497
Dinesh-Gautam <dineshgautam5252@gmail.com>
2021-08-30 22:35:27 +0530

    added scroll to trash component

--- src/components/Trash.jsx
+++ src/components/Trash.jsx
@@ -21,47 +21,50 @@ function Trash({ trashData, displayState, setdisplayState }) {
           text={<UseSvg type="close" />}
         />
       </div>
-      <div className="trash_content">
-        {trashData && trashData.length > 0 ? (
-          trashData.map(({ data, id }) => {
-            const headingText = data.find(
-              (data) => data.name === "heading_input_value"
-            );
-            const DropdownBackgroundColor = data.find(
-              (data) => data.name === "color_input_value"
-            );
-            return (
-              <DropDown
-                key={id}
-                extraButtons={
-                  <button
-                    onClick={() => {
-                      const data = {
-                        delete: false,
-                      };
-                      updateData_firestore(id, data);
-                    }}
-                  >
-                    Restore
-                  </button>
-                }
-                DropdownBackgroundColor={
-                  DropdownBackgroundColor && DropdownBackgroundColor.inputValue
-                }
-                mainText={headingText.inputValue}
-              >
-                {data.some((e) => {
-                  return inputOptions.some(
-                    (option) =>
-                      option.name === e.name && e.name !== "color_input_value"
-                  );
-                }) && <OutputTemplet userData={data} />}
-              </DropDown>
-            );
-          })
-        ) : (
-          <span>Nothing in the Trash</span>
-        )}
+      <div className="trash_content_container">
+        <div className="trash_content">
+          {trashData && trashData.length > 0 ? (
+            trashData.map(({ data, id }) => {
+              const headingText = data.find(
+                (data) => data.name === "heading_input_value"
+              );
+              const DropdownBackgroundColor = data.find(
+                (data) => data.name === "color_input_value"
+              );
+              return (
+                <DropDown
+                  key={id}
+                  extraButtons={
+                    <button
+                      onClick={() => {
+                        const data = {
+                          delete: false,
+                        };
+                        updateData_firestore(id, data);
+                      }}
+                    >
+                      Restore
+                    </button>
+                  }
+                  DropdownBackgroundColor={
+                    DropdownBackgroundColor &&
+                    DropdownBackgroundColor.inputValue
+                  }
+                  mainText={headingText.inputValue}
+                >
+                  {data.some((e) => {
+                    return inputOptions.some(
+                      (option) =>
+                        option.name === e.name && e.name !== "color_input_value"
+                    );
+                  }) && <OutputTemplet userData={data} />}
+                </DropDown>
+              );
+            })
+          ) : (
+            <span>Nothing in the Trash</span>
+          )}
+        </div>
       </div>
     </div>
   );
