c8f3e250e86dba8f9f32ffc44d22ed07e79fe44b
Dinesh-Gautam <dineshgautam5252@gmail.com>
2021-08-31 00:25:19 +0530

    heading expandes verticaly when dropdown is droped

--- src/components/elements/DropDown.js
+++ src/components/elements/DropDown.js
@@ -17,32 +17,33 @@ function DropDown({
   return (
     <div
       // style={{ backgroundColor: DropdownBackgroundColor || "" }}
-      className="dropdown_container"
+      className={`dropdown_container ${dropdownDisplay ? "visible" : false}`}
     >
       <div className="dropdown_wraper">
         <div
           style={{ backgroundColor: DropdownBackgroundColor || "" }}
           className="dropdown_header_side_color"
         ></div>
-        <h1 className={className || "default_dropdown_main_container"}>
-          {mainText}
-        </h1>
+        <div className="dropdown_heading">
+          <h1 className={className || "default_dropdown_main_container"}>
+            {mainText}
+          </h1>
+        </div>
         <div className="dropdown_btn_container">
           {extraButtons}
-          {children && (
-            <button onClick={dropInnerContainer}>
-              <UseSvg type="expand" />
-            </button>
-          )}
+          <button
+            style={{
+              transform: dropdownDisplay ? "rotate(180deg)" : "rotate(0deg)",
+            }}
+            onClick={dropInnerContainer}
+          >
+            <UseSvg type="expand" />
+          </button>
         </div>
       </div>
       <div
         // style={{ display: dropdownDisplay ? "block" : "none" }}
-        className={
-          dropdownDisplay
-            ? "dropdown_inner_container visible"
-            : "dropdown_inner_container"
-        }
+        className="dropdown_inner_container"
       >
         {children}
       </div>
