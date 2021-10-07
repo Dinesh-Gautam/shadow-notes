f16c5737f2d7c811f4ee0bd062d5103c6b7ca04b
Dinesh-Gautam <dineshgautam5252@gmail.com>
2021-10-07 16:17:42 +0530

    made draggable working

--- src/components/MainInput/inputs/elements/List.js
+++ src/components/MainInput/inputs/elements/List.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import UseSvg from "../../../elements/UseSvg";
 import Button from "./Button";
-
+// import { gsap } from "gsap";
+// import { Draggable } from "gsap/all";
 function List({
   index,
   list,
@@ -11,9 +12,66 @@ function List({
   listInputValue,
 }) {
   const [dragDisplay, setdragDisplay] = useState(false);
+  const listDragRef = useRef(null);
+  const listEleRef = useRef(null);
+  const [dragState, setdragState] = useState(false);
+  const dragOverHandler = useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
 
+    const container = e.target.closest(".list_input_box");
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+
+    function getDragAfterElement(container, y) {
+      const draggableElements = [
+        ...container.querySelectorAll(".list-container:not(.dragging)"),
+      ];
+
+      return draggableElements.reduce(
+        (closest, child) => {
+          const box = child.getBoundingClientRect();
+          const offset = y - box.top - box.height / 2;
+          if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+          } else {
+            return closest;
+          }
+        },
+        { offset: Number.NEGATIVE_INFINITY }
+      ).element;
+    }
+  }, []);
+
+  useEffect(() => {
+    // gsap.registerPlugin(Draggable);
+    // Draggable.create(
+    //   listDragRef.current,
+    //   {
+    //     type: "x,y",
+    //     bounds: ".list-container",
+    //     inertia: true,
+    //   },
+    //   []
+    // );
+
+    const dragContainer = listDragRef.current.closest(".list_input_box");
+
+    dragContainer.addEventListener("dragover", dragOverHandler);
+
+    return () => {
+      console.log("clearing drag event");
+      dragContainer.removeEventListener("dragover", dragOverHandler);
+    };
+  }, []);
   return (
     <div
+      ref={listEleRef}
       onMouseEnter={() => {
         setdragDisplay(true);
       }}
@@ -22,8 +80,25 @@ function List({
       }}
       className="list-container"
       id={list.id}
+      draggable={dragState}
+      onDragStart={(e) => {
+        e.target.classList.add("dragging");
+      }}
+      onDragEnd={(e) => {
+        e.target.classList.remove("dragging");
+      }}
     >
-      <div style={{ display: true ? "flex" : "none" }} className="list-drag">
+      <div
+        ref={listDragRef}
+        style={{ display: dragDisplay ? "flex" : "none" }}
+        className="list-drag"
+        onMouseEnter={() => {
+          setdragState(true);
+        }}
+        onMouseLeave={() => {
+          setdragState(false);
+        }}
+      >
         <UseSvg type="drag" />
       </div>
       {React.createElement(list.tag, {
