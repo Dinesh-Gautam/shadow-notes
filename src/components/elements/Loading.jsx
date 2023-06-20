08d8a529c193412f547017d7962201a42a1862f9
Dinesh Gautam <dineshgautam5252@gmail.com>
2023-06-20 07:36:35 +0530

    added module styles"

--- src/components/elements/Loading.jsx
+++ src/components/elements/Loading.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import styles from "./Loading.module.scss";
 function Loading({ type }) {
   switch (type) {
     case "simple-card":
-      return <div className={type}></div>;
+      return <div className={styles[type]}></div>;
     default:
       return "loading...";
   }
