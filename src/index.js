ba36fcd0204cfc077d719607672f3c1547204595
Dinesh-Gautam <dineshgautam5252@gmail.com>
2023-07-14 22:49:04 +0530

    fixed firebase path error

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { AuthProvider } from "./context/AuthContext";
 import { createRoot } from "react-dom/client";
 
 import {
+  BrowserRouter,
   Route,
   RouterProvider,
+  Routes,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
@@ -15,19 +17,15 @@ import Shared from "./components/Shared";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<App />}>
-        {/* ... etc. */}
-      </Route>
-      <Route path="shared/:userId/:docId" element={<Shared />} />
-    </>
-  )
-);
-
 root.render(
   <AuthProvider>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {/* ... etc. */}
+        </Route>
+        <Route path="/shared/:userId/:docId" element={<Shared />} />
+      </Routes>
+    </BrowserRouter>
   </AuthProvider>
 );
