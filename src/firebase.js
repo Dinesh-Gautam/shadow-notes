e05169efc254263356d7c00fa502ef260177a406
Dinesh-Gautam <dineshgautam5252@gmail.com>
2024-03-12 17:39:15 +0530

    finally using env

--- src/firebase.js
+++ src/firebase.js
@@ -3,24 +3,29 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // firebase init
-const app = initializeApp({
-  apiKey: "AIzaSyCq_UQxOD_KUDyrj0BN4UtEJWKHFidfsjw",
-  authDomain: "list-app-react-5252.firebaseapp.com",
-  databaseURL: "https://list-app-react-5252.firebaseio.com",
-  projectId: "list-app-react-5252",
-  storageBucket: "list-app-react-5252.appspot.com",
-  messagingSenderId: "549046639777",
-  appId: "1:549046639777:web:935748e2ff198bd1a1a4e0",
-  measurementId: "G-SH5VGQB72W",
-
-  // development server
-  // apiKey: "AIzaSyB9VgvbdmNX1EmSQtJA69ME2Wqy7svMDYU",
-  // authDomain: "list-app-v2-development.firebaseapp.com",
-  // projectId: "list-app-v2-development",
-  // storageBucket: "list-app-v2-development.appspot.com",
-  // messagingSenderId: "528664552621",
-  // appId: "1:528664552621:web:445439863059ac1dc85224",
-});
+const app = initializeApp(
+  process.env.NODE_ENV === "production"
+    ? {
+        apiKey: process.env.REACT_APP_PROD_API_KEY,
+        authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+        databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+        projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_PROD_APP_ID,
+        measurementId: process.env.REACT_APP_PROD_MEASUREMENT_ID,
+      }
+    : {
+        apiKey: process.env.REACT_APP_DEV_API_KEY,
+        authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+        databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+        projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_DEV_APP_ID,
+        measurementId: process.env.REACT_APP_PROD_MEASUREMENT_ID,
+      }
+);
 
 // firebase auth
 export const auth = getAuth();
