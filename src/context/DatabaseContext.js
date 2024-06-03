4701280db5ebc58ed77bc6773a96c04f914f591a
Dinesh-Gautam <dineshgautam5252@gmail.com>
2024-06-03 15:53:49 +0530

    removed migration code

--- src/context/DatabaseContext.js
+++ src/context/DatabaseContext.js
@@ -20,12 +20,12 @@ import { uuidv4 } from "@firebase/util";
 import { input, listTypes } from "../components/MainInput/inputs/inputOptions";
 import useOutputFilters from "../components/OutputFilters/filterContext";
 
-const data_context = React.createContext();
+const dataContext = React.createContext();
 
 const users = collection(db, "users");
 
 export function useData() {
-  return useContext(data_context);
+  return useContext(dataContext);
 }
 
 let undoInterval = null;
@@ -37,8 +37,6 @@ export function DatabaseContext({ children }) {
   const [trashData, setTrashData] = useState(null);
   const filterData = useOutputFilters();
   const [undoTrigger, setUndoTrigger] = useState({ trigger: false, notes: [] });
-
-  const [loading, setLoading] = useState(true);
 
   const userID = currentUser?.uid || null;
 
@@ -66,119 +64,14 @@ export function DatabaseContext({ children }) {
       where("delete", "==", false),
       orderBy("publishDate", "desc")
     );
-    let unsubscribe = onSnapshot(q, (snapshot) => {
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
       setUserData(
         snapshot.docs.map((doc) => {
           return { id: doc.id, ...doc.data() };
         })
       );
     });
-
-    const isMigrated = JSON.parse(localStorage.getItem("isMigratedToV3"));
-
-    if (!isMigrated) {
-      window.alert("New version available!\nDo you want to migrate your data?");
-
-      migrateV2DataToV3();
-    } else {
-      setLoading(false);
-    }
-
-    async function migrateV2DataToV3() {
-      // get all the documents from the v2 collection
-      const docSnap = await getDoc(userDoc);
-      //check if the data is migrated
-      if (docSnap.exists()) {
-        const data = docSnap.data();
-
-        if (data.version === 3) {
-          console.warn("Data already migrated!");
-          localStorage.setItem("isMigratedToV3", true);
-          setLoading(false);
-          return;
-        }
-      }
-
-      const v2Docs = query(userDocCollection);
-      // get all docs
-
-      getDocs(v2Docs).then((snapshot) => {
-        const allDocs = snapshot.docs;
-        // const json = allDocs.map((e) => e.data());
-        // console.log(JSON.stringify(json));
-        // return;
-        let newData = allDocs.map((doc) => {
-          const newDocData = doc.data().data.map((data) => {
-            let { additionalValue, id, inner, name, value, inputValue } = data;
-            if (name === "Pragraph_input_value") {
-              name = "Paragraph_input_value";
-            }
-
-            if (inner) {
-              // it is a list
-              const innerList = inner.map((innerItem) => {
-                let inputValue = innerItem;
-                let checked = innerItem?.done || false;
-                if (innerItem.value) {
-                  inputValue = innerItem.value;
-                }
-                return {
-                  name,
-                  value,
-                  state: { value: inputValue, checked },
-                  id: uuidv4(),
-                  parentId: id,
-                };
-              });
-              return [
-                {
-                  name,
-                  value,
-                  id,
-                  isFocusable: false,
-                  type: listTypes.checked,
-                },
-                ...innerList,
-              ];
-            }
-            return {
-              name,
-              value,
-              id,
-              nonMoveable: name === input.heading || name === input.color,
-              isRequired: name === input.heading,
-              state: {
-                value: inputValue,
-                labelValue: additionalValue?.labelValue || null,
-              },
-            };
-          });
-
-          const data = doc.data();
-          return { ...data, data: newDocData.flat() };
-        });
-
-        // delete all docs from userDocCollection
-        const deleteDocs = allDocs.map((doc) => {
-          return deleteDoc(doc.ref);
-        });
-
-        // add newData to userDocCollection
-        Promise.all(deleteDocs).then(() => {
-          newData.forEach((eachData) => {
-            setData_fireStore(eachData);
-          });
-
-          window.alert("migrated");
-          localStorage.setItem("isMigratedToV3", true);
-          localStorage.removeItem("inputs");
-          updateDoc(userDoc, {
-            version: 3,
-          });
-          setLoading(false);
-        });
-      });
-    }
 
     return () => {
       unsubscribe();
@@ -235,9 +128,5 @@ export function DatabaseContext({ children }) {
     updateDocField,
     ...filterData,
   };
-  return (
-    <data_context.Provider value={value}>
-      {!loading && children}
-    </data_context.Provider>
-  );
+  return <dataContext.Provider value={value}>{children}</dataContext.Provider>;
 }
